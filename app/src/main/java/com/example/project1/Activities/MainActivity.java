4e4ba06e0b8a75ac3df7ad3e68cbb553a8c7b43b
Trong Nhan <trongnhannguyen94@gmail.com>
2020-03-19 11:02:21 +0700

    Add package Activities

--- app/src/main/java/com/example/project1/Activities/MainActivity.java
+++ app/src/main/java/com/example/project1/Activities/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.project1;
+package com.example.project1.Activities;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
@@ -14,6 +14,7 @@ import com.example.project1.Fragment.FragmentCheckIn;
 import com.example.project1.Fragment.FragmentChoO;
 import com.example.project1.Fragment.FragmentDoiMatKhau;
 import com.example.project1.Fragment.FragmentTravelBlog;
+import com.example.project1.R;
 import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
