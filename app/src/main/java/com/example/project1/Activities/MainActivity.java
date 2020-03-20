2a7dd561b48626688137cefe9d4e58e777544ed1
Trong Nhan <trongnhannguyen94@gmail.com>
2020-03-20 09:24:43 +0700

    Add Firebase to Project

--- app/src/main/java/com/example/project1/Activities/MainActivity.java
+++ app/src/main/java/com/example/project1/Activities/MainActivity.java
@@ -17,6 +17,8 @@ import com.example.project1.Fragment.FragmentDoiMatKhau;
 import com.example.project1.Fragment.FragmentTravelBlog;
 import com.example.project1.R;
 import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     NavigationView navigationView;
@@ -28,7 +30,9 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        Toast.makeText(this, "Tran Quoc Hao", Toast.LENGTH_SHORT).show();
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+        myRef.setValue("Hello, World!");
         initView();
     }
 
